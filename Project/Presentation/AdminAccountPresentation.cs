f4fdf6bcb2372e0714d3bc7686f79fe17e7d78c0
Basel-1 <159136975+Basel-1@users.noreply.github.com>
2024-10-18 14:01:17 +0200

    Improve admin login

--- Project/Presentation/AdminAccountPresentation.cs
+++ Project/Presentation/AdminAccountPresentation.cs
@@ -1,30 +1,41 @@
 using System.Collections.Concurrent;
 using System.Data.Common;
-
+using System.Threading;
 static class AdminAccountPresentation
 {
     static AdminAccountLogic logic = new AdminAccountLogic();
     public static void Login()
     {
-        Console.WriteLine("Enter Your Username: ");
-        string username = Console.ReadLine();
-
-        Console.WriteLine("Enter Your password: ");
-        string password = Console.ReadLine();
-
-        bool isValid = logic.ValidateLogin(username, password);
-
-        if (isValid)
+        int i = 0;
+        while (true)
         {
-            Console.WriteLine("Login as Admin successful. Welcome!");
 
+            Console.WriteLine("Enter Your Username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter Your password: ");
+            string password = Console.ReadLine();
+
+            bool isValid = logic.ValidateLogin(username, password);
+
+            if (isValid)
+            {
+                Console.WriteLine("Login as Admin successful. Welcome!");
+
+            }
+            else
+            {
+                i++;
+                Console.WriteLine("Invalid email or password. Please try again.");
+                if (i >= 3)
+                {
+                    Console.WriteLine("You will be locked out for 1 minute due to multiple failed attempts.");
+                    Thread.Sleep(60000);
+                    i = 0;
+                }
+
+            }
         }
-        else
-        {
-            Console.WriteLine("Invalid email or password. Please try again.");
-
-        }
-
 
     }
 
