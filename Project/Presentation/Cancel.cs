92ffe52d5c367b148b2760001484ae4d28509498
user02300 <1060594@hr.nl>
2024-10-19 17:39:47 +0200

    main class

---
+++ Project/Presentation/Cancel.cs
@@ -0,0 +1,44 @@
+using System;
+
+static class Cancel 
+{
+    static void Main()
+    {
+        CancelFlight cancel = new CancelFlight();
+
+        while (true)
+        {
+            if (cancel.Booked.Count > 0) 
+            {
+                Console.WriteLine("Booked Flights:");
+                Console.WriteLine(cancel.BookedFlights()); 
+
+                Console.WriteLine("Enter 'C' to cancel a flight or 'Q' to quit:");
+                string input = Console.ReadLine().ToUpper(); 
+
+                if (input == "Q")
+                {
+                    Console.WriteLine("Quitting the program");
+                    break; 
+                }
+
+                if (input == "C")
+                {
+                    Console.WriteLine("Enter the index of the flight you want to cancel:");
+
+                    string indexinput = Console.ReadLine();
+
+                    int index = Convert.ToInt32(indexinput);
+                        
+                    cancel.CancelFlights(index); 
+
+                }
+            }
+            else 
+            {
+                Console.WriteLine("You have no flights booked.");
+                break; 
+            }
+        }
+    }
+}
