fb92fa3d9f443b82e43e32f39ca89627712a8d0a
user02300 <1060594@hr.nl>
2024-11-12 21:22:59 +0100

    prog

---
+++ Project/Presentation/CancelPres.cs
@@ -0,0 +1,28 @@
+public class CancelPres
+{
+    
+// public static string AvailableSeats(LayoutModel flight)
+//     {
+//         List<string> availableSeats = flight.AvailableSeats;
+
+
+//         if (availableSeats.Count > 0)
+//         {
+//             return string.Join(", ", availableSeats);
+//         }
+//         else
+//         {
+//             return null;
+//         }
+//     }
+
+
+
+
+
+
+
+
+
+
+}
