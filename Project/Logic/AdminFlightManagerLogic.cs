2cab73dab40cbae8fd30f00b86306a477b4fd439
Basel-1 <159136975+Basel-1@users.noreply.github.com>
2024-11-19 14:17:52 +0100

    Changes

--- Project/Logic/AdminFlightManagerLogic.cs
+++ Project/Logic/AdminFlightManagerLogic.cs
@@ -12,10 +12,24 @@ namespace DataAccess
     {
         public static void LaodFlight()
         {
+            Console.Clear();
             var flights = FlightsAccess.ReadAll();
             foreach (var flight in flights)
             {
-                Console.WriteLine($"Flight ID: {flight.Id}, Airline: {flight.Airline}, TicketPrice: {flight.TicketPrice}, Gate: {flight.Gate}, DepartureAirport: {flight.DepartureAirport}, ArrivalDestination: {flight.ArrivalDestination}, IsCancelled: {flight.IsCancelled}, DepartureDate: {flight.DepartureDate}, FlightTime: {flight.FlightTime}, AvailableSeats: {flight.AvailableSeats}, FlightPoints: {flight.FlightPoints}");
+
+                // Console.WriteLine($"Flight ID: {flight.Id}, Airline: {flight.Airline}, TicketPrice: {flight.TicketPrice}, Gate: {flight.Gate}, DepartureAirport: {flight.DepartureAirport}, ArrivalDestination: {flight.ArrivalDestination}, IsCancelled: {flight.IsCancelled}, DepartureDate: {flight.DepartureDate}, FlightTime: {flight.FlightTime}, AvailableSeats: {flight.AvailableSeats}, FlightPoints: {flight.FlightPoints}");
+                Console.WriteLine($"Flight ID: {flight.Id}");
+                Console.WriteLine($"Airline: {flight.Airline}");
+                Console.WriteLine($"Ticket Price: {flight.TicketPrice}");
+                Console.WriteLine($"Gate: {flight.Gate}");
+                Console.WriteLine($"Departure Airport: {flight.DepartureAirport}");
+                Console.WriteLine($"Arrival Destination: {flight.ArrivalDestination}");
+                Console.WriteLine($"Is Cancelled: {flight.IsCancelled}");
+                Console.WriteLine($"Departure Date: {flight.DepartureDate}");
+                Console.WriteLine($"Flight Time: {flight.FlightTime}");
+                Console.WriteLine($"Available Seats: {flight.AvailableSeats}");
+                Console.WriteLine($"Flight Points: {flight.FlightPoints}");
+                Console.WriteLine("-----------------------------------------");
             }
         }
         // public static List<FlightModel> allFlights = FlightsAccess.ReadAll();
