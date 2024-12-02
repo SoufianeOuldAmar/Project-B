91b21c8148e4dad9a024eadd879f560cb4a5bf25
mtsahinhr <1065791@hr.nl>
2024-12-02 15:43:54 +0100

    Added User information function.

--- Project/Logic/PassengerLogic.cs
+++ Project/Logic/PassengerLogic.cs
@@ -1,69 +1,110 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using DataModels;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using DataModels;
 
-namespace BusinessLogic
-{
-    public static class PassengerLogic
-    {
-        public static List<PassengerModel> CollectPassengerDetails(int numberOfPassengers)
-        {
-            var passengers = new List<PassengerModel>();
+// namespace BusinessLogic
+// {
+//     public static class PassengerLogic
+//     {
+//         public static List<PassengerModel> CollectPassengerDetails(int numberOfPassengers)
+//         {
+//             var passengers = new List<PassengerModel>();
 
-            for (int i = 0; i < numberOfPassengers; i++)
-            {
-                Console.WriteLine($"=== Enter details for Passenger {i + 1} ===");
-                PassengerModel passenger = new PassengerModel();
+//             for (int i = 0; i < numberOfPassengers; i++)
+//             {
+//                 Console.WriteLine($"=== Enter details for Passenger {i + 1} ===");
+//                 PassengerModel passenger = new PassengerModel();
 
-                Console.Write("First Name: ");
-                passenger.FirstName = Console.ReadLine();
+//                 // Collect and validate first name
+//                 while (true)
+//                 {
+//                     Console.Write("First Name: ");
+//                     string firstName = Console.ReadLine();
+//                     if (!string.IsNullOrWhiteSpace(firstName) && firstName.All(char.IsLetter))
+//                     {
+//                         passenger.FirstName = firstName;
+//                         break;
+//                     }
+//                     Console.WriteLine("Invalid First Name. Please enter alphabets only.");
+//                 }
 
-                Console.Write("Last Name: ");
-                passenger.LastName = Console.ReadLine();
+//                 // Collect and validate last name
+//                 while (true)
+//                 {
+//                     Console.Write("Last Name: ");
+//                     string lastName = Console.ReadLine();
+//                     if (!string.IsNullOrWhiteSpace(lastName) && lastName.All(char.IsLetter))
+//                     {
+//                         passenger.LastName = lastName;
+//                         break;
+//                     }
+//                     Console.WriteLine("Invalid Last Name. Please enter alphabets only.");
+//                 }
 
-                Console.Write("Title (Mr/Ms/Dr): ");
-                passenger.Title = Console.ReadLine();
+//                 // Collect and validate title
+//                 while (true)
+//                 {
+//                     Console.Write("Title (Mr/Ms/Dr): ");
+//                     string title = Console.ReadLine();
+//                     if (new[] { "Mr", "Ms", "Dr" }.Contains(title))
+//                     {
+//                         passenger.Title = title;
+//                         break;
+//                     }
+//                     Console.WriteLine("Invalid Title. Please choose from (Mr, Ms, Dr).");
+//                 }
 
-                Console.Write("Age Group (adult/child/infant): ");
-                passenger.AgeGroup = Console.ReadLine().ToLower();
+//                 // Collect and validate age group
+//                 while (true)
+//                 {
+//                     Console.Write("Age Group (adult/child/infant): ");
+//                     string ageGroup = Console.ReadLine().ToLower();
+//                     if (new[] { "adult", "child", "infant" }.Contains(ageGroup))
+//                     {
+//                         passenger.AgeGroup = ageGroup;
 
-                if (passenger.AgeGroup == "infant")
-                {
-                    Console.Write("Date of Birth (yyyy-MM-dd): ");
-                    string dobInput = Console.ReadLine();
-                    if (DateTime.TryParse(dobInput, out DateTime dob))
-                    {
-                        passenger.DateOfBirth = dob;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid date format. Please try again.");
-                        i--; // Repeat for the same passenger
-                        continue;
-                    }
-                }
+//                         // Validate date of birth for infants
+//                         if (ageGroup == "infant")
+//                         {
+//                             while (true)
+//                             {
+//                                 Console.Write("Date of Birth (yyyy-MM-dd): ");
+//                                 string dobInput = Console.ReadLine();
+//                                 if (DateTime.TryParse(dobInput, out DateTime dob) && dob <= DateTime.Now)
+//                                 {
+//                                     passenger.DateOfBirth = dob;
+//                                     break;
+//                                 }
+//                                 Console.WriteLine("Invalid Date of Birth. Please enter a valid past date.");
+//                             }
+//                         }
+//                         break;
+//                     }
+//                     Console.WriteLine("Invalid Age Group. Please enter 'adult', 'child', or 'infant'.");
+//                 }
 
-                passengers.Add(passenger);
-            }
+//                 passengers.Add(passenger);
+//             }
 
-            return passengers;
-        }
+//             return passengers;
+//         }
 
-        public static void ValidatePassengerDetails(List<PassengerModel> passengers)
-        {
-            foreach (var passenger in passengers)
-            {
-                if (string.IsNullOrWhiteSpace(passenger.FirstName) || string.IsNullOrWhiteSpace(passenger.LastName))
-                {
-                    throw new ArgumentException("First Name and Last Name cannot be empty.");
-                }
 
-                if (passenger.AgeGroup == "infant" && passenger.DateOfBirth == null)
-                {
-                    throw new ArgumentException("Infants must have a valid Date of Birth.");
-                }
-            }
-        }
-    }
-}
+//         public static void ValidatePassengerDetails(List<PassengerModel> passengers)
+//         {
+//             foreach (var passenger in passengers)
+//             {
+//                 if (string.IsNullOrWhiteSpace(passenger.FirstName) || string.IsNullOrWhiteSpace(passenger.LastName))
+//                 {
+//                     throw new ArgumentException("First Name and Last Name cannot be empty.");
+//                 }
+
+//                 if (passenger.AgeGroup == "infant" && passenger.DateOfBirth == null)
+//                 {
+//                     throw new ArgumentException("Infants must have a valid Date of Birth.");
+//                 }
+//             }
+//         }
+//     }
+// }
